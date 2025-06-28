d987065a043dfe78651acedc52315e6c1452adc5
Julle98 <152638153+Julle98@users.noreply.github.com>
2025-06-28 22:15:57 +0300

    Newest update to the repository

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from dashboard.api import app
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "bot"))
 from bot.main import load_cogs
 
 async def main():
